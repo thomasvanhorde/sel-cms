1931ff21e3345d0daae36a775c6c6bead7874215
thomas.vanhorde@gmail.com <thomas.vanhorde@gmail.com@3e78b107-4a32-fdac-a73a-0a3444641bc0>
2011-02-15 18:27:52 +0000

    ContentManager update

--- trunk/engine/inc/class/ContentManager/ContentStruct.class.php
+++ trunk/engine/inc/class/ContentManager/ContentStruct.class.php
@@ -2,42 +2,76 @@
 
 class ContentStruct {
 
-    private $_data;
-
+    private $_dataSite;
+    private $_dataAll;
+    
     function __construct(){
         $this->load();
     }
 
     function load(){
-        foreach($this->loadXml()->children() as $k => $e)
-            $this->_data[(string)$e['id']] = $e;
+        foreach($this->loadXmlEngine()->children() as $k => $e){
+            $this->_dataAll[(string)$e['id']] = $e;
+        }
+        foreach($this->loadXmlSite()->children() as $k => $e){
+            $this->_dataSite[(string)$e['id']] = $e;
+            $this->_dataAll[(string)$e['id']] = $e;
+        }
     }
 
-    function loadXml(){
-        return simplexml_load_file(ENGINE_URL.FOLDER_INC.INFOS_XML_CONTENT_STRUCT, NULL, true);    
+    function loadXmlEngine(){
+        return simplexml_load_file(ENGINE_URL.FOLDER_INC.FOLDER_CONTENT_MANAGER.INFOS_XML_CONTENT_STRUCT, NULL, true);
+    }
+
+    function loadXmlSite(){
+        return simplexml_load_file(FOLDER_INC.INFOS_XML_CONTENT_STRUCT, NULL, true);    
     }
 
     function get(){
-        return $this->_data;
+        return $this->_dataSite;
+    }
+
+    function getAll(){
+        return $this->_dataAll;
+    }
+
+    function delete($uid){
+        $out = $Xml = $this->loadXmlSite();
+        $i = 0;
+        foreach($Xml as $k => $d){
+           if($d[@id] == $uid && $d[@locked] == 'false')
+               unset($out->element->$i);
+           $i++;
+        }
+        $out->saveXML(FOLDER_INC.INFOS_XML_CONTENT_STRUCT);
     }
 
     function save($data){
-        $Xml = $this->loadXml();
+        $Xml = $this->loadXmlSite();
         $xmlArray = $this->get();
 
-        $i = 0;
-        foreach($xmlArray as $k => $d){
-            if((string)$d[@id] == $data['id']){
-                $uid = (string)$d[@id];
-                unset($Xml->element->$i);
-                break;
+        if(isset($data['id']) && !empty($data['id'])){ // If id = edit
+            $i = 0;
+            foreach($xmlArray as $k => $d){
+                if((string)$d[@id] == $data['id']){
+                    $uid = (string)$d[@id];
+                    unset($Xml->element->$i);
+                    break;
+                }
+                $i++;
             }
-            $i++;
         }
-
+        else {  // else = new
+            $uid = 0;
+            foreach($xmlArray as $k => $d){
+                if((string)$d[@id] >= $uid)
+                    $uid = (string)$d[@id] + 1;
+            }
+        }
 
         $newNode = $Xml->addChild('element');
         $newNode->addAttribute('id', $uid);
+        $newNode->addAttribute('locked', 'false');
         $newNode->addChild('name', $data['name']);
         $newNode->addChild('description', $data['description']);
         $newNodeTypes = $newNode->addChild('types');
@@ -52,9 +86,9 @@ class ContentStruct {
             }
         }
 
-        $Xml->saveXML(ENGINE_URL.FOLDER_INC.INFOS_XML_CONTENT_STRUCT);
+        $Xml->saveXML(FOLDER_INC.INFOS_XML_CONTENT_STRUCT);
+
+        return $uid;
     }
 
 }
-
-?>
